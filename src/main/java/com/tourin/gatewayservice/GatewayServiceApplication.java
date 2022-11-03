f7998fcd16a2e5493f58ef9350411dacd3e5ae0d
Imane-Rahoui <Imaanerahoui@gmail.com>
2022-11-03 22:29:23 +0100

    Semi dynamic

--- src/main/java/com/tourin/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/tourin/gatewayservice/GatewayServiceApplication.java
@@ -13,11 +13,12 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-    @Bean
+    @Bean         // si on connait le nom des microservices :
     RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+
         return routeLocatorBuilder.routes()
-                .route(r->r.path("/customers/**").uri("http://localhost:8081/")) // les nvelle versions pas d id
-                .route(r->r.path("/products/**").uri("http://localhost:8082/"))
+                .route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE")) // les nvelle versions pas d id
+                .route(r->r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
                 .build();
     }
 }
