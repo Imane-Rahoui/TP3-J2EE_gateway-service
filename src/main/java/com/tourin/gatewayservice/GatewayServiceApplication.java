ca855efa74bc670ebeb1bab67d34ed28b5e96ad0
Imane-Rahoui <Imaanerahoui@gmail.com>
2022-11-03 22:19:22 +0100

    static config

---
+++ src/main/java/com/tourin/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,23 @@
+package com.tourin.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(r->r.path("/customers/**").uri("http://localhost:8081/")) // les nvelle versions pas d id
+                .route(r->r.path("/products/**").uri("http://localhost:8082/"))
+                .build();
+    }
+}
